83c4c79581c94516fcbf1b34a5844a6131239afc
sngk <andrey.udodov@me.com>
2018-10-24 14:01:57 +1100

    v3_GUI

--- game_finder.py
+++ game_finder.py
@@ -21,15 +21,67 @@ class ProgramGUI:
 
     def __init__(self):
         # This is the constructor of the class.
-        # It is responsible for loading and reading the data from the text file and creating the user interface.
-        # See the "Constructor of the GUI Class of game_finder.py" section of the assignment brief.
+        try:
+            with open('data.txt') as file:
+                self.data = json.load(file)
+
+        except IOError:
+            tkinter.messagebox.showerror('Error!', 'Missing File.')
+            self.data = []
+            self.main.destroy()
+            return
+        except ValueError:
+            self.data = []
+            tkinter.messagebox.showerror('Error!', 'Invalid File.')
+            self.main.destroy()
+            return
+        self.main = tkinter.Tk()
+
+        self.main.title("Game Finder")
+
+        self.constraints = tkinter.Frame(self.main)
+        self.matchingGames = tkinter.Frame(self.main)
+
+        tkinter.Label(self.main, text='Constraints:').grid(columnspan=3)
+
+        tkinter.Label(self.main, text='Number of players: ').grid(row=1, column=0)
+        tkinter.Label(self.main, text='Time available (mins): ').grid(row=2, column=0)
+        tkinter.Label(self.main, text='Age of youngest player: ').grid(row=3, column=0)
+
+        self.youngestAge = tkinter.StringVar()
+        self.playersNum = tkinter.StringVar()
+        self.timeAvail = tkinter.StringVar()
+
+        tkinter.Entry(self.main, textvariable=self.playersNum, width=10).grid(row=1, column=1)
+        tkinter.Entry(self.main, textvariable=self.timeAvail, width=10).grid(row=2, column=1)
+        tkinter.Entry(self.main, textvariable=self.youngestAge, width=10).grid(row=3, column=1)
+
+        self.submitButton = tkinter.Button(self.main, text='Submit', command=self.greet)
+        self.submitButton.grid(row=4, columnspan=3)
+
+        # self.criteriaLabel = tkinter.StringVar()
+
+        tkinter.Label(self.main, text='Matching Games: ').grid(row=6, columnspan=3)
+        self.criteriaLabel = tkinter.Label(self.main, text='No criteria submitted.', justify='center')
+        self.criteriaLabel.grid(row=7, columnspan=3)
+        tkinter.Button(self.main, text='Quit', command=self.greet).grid(row=10, columnspan=3)
+
+        tkinter.mainloop()
+
+    def greet(self):
+        print("Greetings!")
+        self.criteriaLabel.config(text='Hello :D')
+        if len(self.youngestAge.get() + self.playersNum.get() + self.timeAvail.get()) == 0:
+            tkinter.messagebox.showerror('Error!', 'enter data')
+        else:
+            # text = self.data[0]
+            # self.criteriaLabel.config(text)
         pass
 
     def findGames(self):
         # This method finds and displays games matching the criteria entered by the user.
         # See the "The findGames() Method of the GUI Class of game_finder.py" section of the assignment brief.
         pass
-
 
 
 # Create an object of the ProgramGUI class to begin the program.
