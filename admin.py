9e4d6ab7a93241a86992832811d0ebcc2d5eba92
sngk <andrey.udodov@me.com>
2019-08-19 10:22:24 +1000

    comments added; minor fixes.

--- admin.py
+++ admin.py
@@ -7,6 +7,7 @@ import json
 
 
 # This function repeatedly prompts for input until an integer is entered.
+# If the invalid input  -- displays an error and asks for another try
 def inputInt(prompt, errorMessage='Invalid input - Try again.', minValue=None, maxValue=None):
     while True:
         value = input(prompt)
@@ -25,6 +26,7 @@ def inputInt(prompt, errorMessage='Invalid input - Try again.', minValue=None, m
 
 
 # This function repeatedly prompts for input until something other than whitespace is entered.
+# If nothing entered displays an error
 def inputSomething(prompt):
     while True:
         value = input(prompt).strip()
@@ -35,7 +37,6 @@ def inputSomething(prompt):
             return value
 
 # This function opens "data.txt" in write mode and writes the data to it in JSON format.
-# See Point 3 of the "Functions in admin.py" section of the assignment brief.
 
 
 def saveData(dataList):
@@ -64,14 +65,18 @@ while True:
     if choice[0] == 'a':
         # Add a new game.
         gameName = inputSomething('Enter the game name > ')
-        nameList = []
+        nameList = []  # create an empty list to use it later for checking if game already exists
         for counter, game in enumerate(data):
             name = game['name'].lower()
             nameList.append(name)
+            # check if game exists, and displays a proper message
+            # the message contains the name of the existing game and its index
         if gameName.lower() in nameList:
             index = nameList.index(gameName.lower())
             print('The game already exists.')
             print('Index: {}; Game name: {}'.format(index, data[index]['name']))
+            # if no such game exists, asks user to provide more information about the game
+            # and saves it
         else:
             minPlayers = inputInt('Enter the minimum players for the game > ', minValue=1)
             maxPlayers = inputInt('Enter the maximum players for the game > ', minValue=minPlayers)
@@ -84,6 +89,8 @@ while True:
 
     elif choice[0] == 'l':
         # List the current games.
+        # if the data.txt file is empty displays the message that there are no games saved
+        # Otherwise, displays the game's index and name
         if not data:
             print('No Games Saved!')
         else:
@@ -93,12 +100,17 @@ while True:
 
     elif choice[0] == 's':
         # Search the current games.
+        # reads user input and based on it desides what to display
         if not data:
             print('No Games Saved!')
         else:
+            # if user input is just one character -- asks for more details like the game name to look for
             if len(choice) == 1:
                 gameName = inputSomething('Enter the name of the game you would like to look for > ').lower()
             else:
+                # if user input is more than 1 character, strips the input and search the game based on what user's entered
+                # if user's input matches the game name in data.txt displays the message that contains game name and its index
+                # if nothing found, displays the message
                 gameName = ' '.join(choice[1:]).strip().lower()
             print('Searching results: ')
             noMatches = True
@@ -112,13 +124,18 @@ while True:
 
     elif choice[0] == 'v':
         # View a game.
+        # reads user input and based on it desides what to display
         if not data:
             print('No Games Saved!')
         else:
             try:
+                # if user input is just one character -- asks for game's index to display information about the game
                 if len(choice) == 1:
                     value = inputInt('Game number (index) to view > ', minValue=0)
                 else:
+                    # if user input is more than 1 character, gets the second character (should be a number) and do the following
+                    # if index that user entered exists in data.txt, displays game information
+                    # if index is out of range or not a number, displays the error messages
                     value = int(choice[1])
                 print(data[value]['name'])
                 print('\tPlayers: {} - {}'.format(data[value]['min_players'], data[value]['max_players']))
@@ -127,7 +144,7 @@ while True:
             except ValueError:
                 print('Invalid input - Try again. Index should be integer number.')
             except IndexError:
-                print('Invalid index number - Out of range.')
+                print('Invalid index number - Out of range. There is total of {} games saved.'.format(len(data)))
 
     elif choice[0] == 'd':
         # Delete a game.
@@ -135,9 +152,13 @@ while True:
             print('No Games Saved!')
         else:
             try:
+                # if user input is just one character -- asks for game's index to delete
                 if len(choice) == 1:
                     value = inputInt('Game number (index) to delete > ', minValue=0)
                 else:
+                    # if user input is more than 1 character, gets the second character (should be a number) and do the following
+                    # if index that user entered exists in data.txt, deletes the recording
+                    # if index is out of range or not a number, displays the error messages
                     value = int(choice[1])
                 del data[value]
                 saveData(data)
@@ -149,6 +170,8 @@ while True:
 
     elif choice[0] == 'b':
         # Display breakdown
+        # the breakdown consists of:
+        # Total number of games, maximum and minimum players across all games, average duration of game, average minimum age
         if not data:
             print('No Games Saved!')
         else:
--- game_finder.py
+++ game_finder.py
@@ -11,6 +11,8 @@ class ProgramGUI:
 
     def __init__(self):
         # This is the constructor of the class.
+        # try to open the file and display error messages if there is no such file or
+        # its data not JSON formatted
         try:
             with open('data.txt') as file:
                 self.data = json.load(file)
@@ -30,6 +32,8 @@ class ProgramGUI:
         self.main.title("Game Finder")
         self.btnFrame = None
 
+        # create labels for constraints
+        # and place them using grid
         tkinter.Label(self.main, text='Constraints:').grid(columnspan=3)
 
         tkinter.Label(self.main, text='Number of players: ').grid(row=1, column=0)
@@ -40,19 +44,24 @@ class ProgramGUI:
         self.matchingLabel = tkinter.Label(self.main, text='No criteria submitted.', justify='center')
         self.matchingLabel.grid(row=7, columnspan=3)
 
+        # stores the data from Entry widgets
         self.youngestAge = tkinter.StringVar()
         self.playersNum = tkinter.StringVar()
         self.timeAvail = tkinter.StringVar()
 
+        # create Entry widgets for players number, available time and the youngest age for the game
         tkinter.Entry(self.main, textvariable=self.playersNum, width=5).grid(row=1, column=1)
         tkinter.Entry(self.main, textvariable=self.timeAvail, width=5).grid(row=2, column=1)
         tkinter.Entry(self.main, textvariable=self.youngestAge, width=5).grid(row=3, column=1)
 
+        # widged for Submit button, that triggers the function to find games with sepcified criteria
         self.submitButton = tkinter.Button(self.main, text='Submit', command=self.findGames)
         self.submitButton.grid(row=4, columnspan=3)
 
         tkinter.mainloop()
 
+# Method to display game information if game button clicked
+# show the message box with the information about the game
     def gameInfo(self, btnIndex):
         message = '{}\n Players: {} - {}\n Duration: {}\n Minimum Age: {}'.format(self.data[btnIndex]['name'], self.data[btnIndex]
                                                                                   ['min_players'], self.data[btnIndex]['max_players'],
@@ -60,20 +69,24 @@ class ProgramGUI:
                                                                                   self.data[btnIndex]['min_age'])
         tkinter.messagebox.showinfo(self.data[btnIndex]['name'], message)
 
+# This method finds and displays games matching the criteria entered by the user.
     def findGames(self):
-        # This method finds and displays games matching the criteria entered by the user.
         try:
             youngestAge = int(self.youngestAge.get())
             timeAvail = int(self.timeAvail.get())
             playersNum = int(self.playersNum.get())
             noMatches = True
             self.buttons = {}
-            if self.btnFrame:  # Exists?
+            if self.btnFrame:  # if button frame Exists remove it (to clear the space)
                 self.btnFrame.grid_remove()
 
             buttonFrame = self.btnFrame = tkinter.Frame(self.main)
             buttonFrame.grid(columnspan=3)
 
+            # Check if there are games in data.txt that match the specified criteria enteterd by the user
+            # if there are no games, displays the "not found" message
+            # if there are games, displays how many of them are there out of total number of games
+            # displays the name of the games as buttons that can be clicked for more information about particular game
             for game in self.data:
                 if (game['min_players'] <= playersNum <= game['max_players']) and (timeAvail >= game['duration']) and (youngestAge >= game['min_age']):
                     btnIndex = self.data.index(game)
